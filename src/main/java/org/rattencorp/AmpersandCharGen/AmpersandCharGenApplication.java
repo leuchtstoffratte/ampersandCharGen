c311dea2dd3beb08e6802bbd7fac1d5655667bf9
leuchtstoffratte <kitersquirel@web.de>
2024-10-15 16:07:26 +0200

    Inital commit with initializr generated project

---
+++ src/main/java/org/rattencorp/AmpersandCharGen/AmpersandCharGenApplication.java
@@ -0,0 +1,15 @@
+package org.rattencorp.AmpersandCharGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpersandCharGenApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(AmpersandCharGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rattencorp/AmpersandCharGen/AmpersandCharGenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rattencorp.AmpersandCharGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpersandCharGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
