af0ec41ba19cee1da32caf9e25d549ac07256fc3
leuchtstoffratte <kitersquirel@web.de>
2024-10-25 21:46:11 +0200

    Refactoring Random-Attribute-Generation II: Employ newly created die-classes in attribute generation. Testability remains an issue

--- src/main/java/org/rattencorp/ampersandCharGen/core/attributes/valuegens/AmpersandAttributeValueGeneratorConfig.java
+++ src/main/java/org/rattencorp/ampersandCharGen/core/attributes/valuegens/AmpersandAttributeValueGeneratorConfig.java
@@ -2,10 +2,12 @@ package org.rattencorp.ampersandCharGen.core.attributes.valuegens;
 
 import org.rattencorp.ampersandCharGen.core.attributes.AmpersandAttribute;
 import org.rattencorp.core.attributes.AttributeValueGenerator;
+import org.rattencorp.core.dice.D6;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class AmpersandAttributeValueGeneratorConfig {
@@ -14,7 +16,6 @@ public class AmpersandAttributeValueGeneratorConfig {
      * Option for swapping out value generation
      */
     private static final String ATTRIBUTE_GENERATOR_TYPE = "attribute.generatorType";
-
 
     static final String BASIC_RANDOM = "random";
     static final String THREE_D6 = "3D6";
@@ -25,7 +26,7 @@ public class AmpersandAttributeValueGeneratorConfig {
         var attribGeneratorType = context.getEnvironment().getProperty(ATTRIBUTE_GENERATOR_TYPE, "None");
 
         return switch (attribGeneratorType){
-            case THREE_D6, BASIC_RANDOM -> new BasicRandomAmpersandAttributesValueGeneratorImpl();
+            case THREE_D6, BASIC_RANDOM -> new BasicRandomAmpersandAttributesValueGeneratorImpl(new D6());
 
             default -> new DefaultAmpersantAttributeValueGeneratorImpl();
 
--- src/main/java/org/rattencorp/ampersandCharGen/core/attributes/valuegens/BasicRandomAmpersandAttributesValueGeneratorImpl.java
+++ src/main/java/org/rattencorp/ampersandCharGen/core/attributes/valuegens/BasicRandomAmpersandAttributesValueGeneratorImpl.java
@@ -3,21 +3,25 @@ package org.rattencorp.ampersandCharGen.core.attributes.valuegens;
 
 import org.rattencorp.ampersandCharGen.core.attributes.AmpersandAttribute;
 import org.rattencorp.core.attributes.AttributeValueGenerator;
+import org.rattencorp.core.dice.D6;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Random;
 
 public class BasicRandomAmpersandAttributesValueGeneratorImpl implements AttributeValueGenerator<AmpersandAttribute> {
 
 
-    private static final Random RANDOM = new Random();
+    private final D6 d6;
 
 
+    BasicRandomAmpersandAttributesValueGeneratorImpl (@Autowired D6 d6) {
+        this.d6 = d6;
+    }
 
     @Override
     public int[] generateValues() {
         var result = new int[6];
 
-        for (int i : result) {
+        for (int i = 0; i < 6; i++) {
             result[i] = generateSingleValue();
         }
 
@@ -27,15 +31,15 @@ public class BasicRandomAmpersandAttributesValueGeneratorImpl implements Attribu
 
     /**
      * roll 4 d6, discard the lowest and
-     * @return
+     * return sum of remaining values
      */
     private int generateSingleValue(){
-        int min = d6Result();
+        int min = d6.roll();
 
         int result = 0;
 
         for (int i = 0; i<3; i++){
-            int next = d6Result();
+            int next = d6.roll();
 
             result += Math.max(min, next);
             min = Math.min(min, next);
@@ -44,9 +48,5 @@ public class BasicRandomAmpersandAttributesValueGeneratorImpl implements Attribu
         return result;
     }
 
-
-    private int d6Result(){
-        return RANDOM.nextInt(1,7);
-    }
 
 }
